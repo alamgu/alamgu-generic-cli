5ee9a693e091aa245e0f5bf7c78f4af24658ce23
John Ericson <John.Ericson@Obsidian.Systems>
2023-02-16 23:46:16 +0000

    Redo build with yarn2nix and IFD

--- default.nix
+++ default.nix
@@ -1,47 +1,133 @@
-{pkgs ? import <nixpkgs> {
-    inherit system;
-}, system ? builtins.currentSystem}:
+{ pkgs ? import (import dep/alamgu/thunk.nix + "/dep/nixpkgs") {}
+, nodejs ? pkgs.nodejs
+}:
 
 let
-  nodePackages = import ./composed.nix {
-    inherit pkgs system;
-  };
-    postInstallFixup = ''
-      # Fixup for checkouts from git.
-        echo "POST INSTALL FIXUP RUNNING"
-        for pkg in hw-app-alamgu
-        do
-        if [ -d node_modules/$pkg ]
-        then pushd node_modules/$pkg; npm run prepare; popd;
-        fi
-        done
+  inherit (pkgs) lib;
+  yarn2nix = import ./dep/yarn2nix { inherit pkgs; };
+  inherit (import (import ./dep/alamgu/thunk.nix) {}) thunkSource;
+  yarnDepsNix = pkgs.runCommand "yarn-deps.nix" {} ''
+    ${yarn2nix}/bin/yarn2nix --offline \
+      <(sed -e '/hw-app-alamgu/,/^$/d' ${./yarn.lock}) \
+      > $out
   '';
-in
-nodePackages // {
-  shell = (nodePackages.shell.override (old: {
-    buildInputs = old.buildInputs ++ [ pkgs.pkg-config pkgs.libusb ];
-    preFixup = ''
-      pushd $out/lib/
-      ${postInstallFixup}
-      popd
-    '';
-      })).overrideAttrs (attrs: {
-          shellHook = attrs.shellHook + ''
-          export TS_NODE_COMPILER_OPTIONS="{\"baseUrl\": \"$NODE_PATH\"}"
-          '';
-      });
-  package = nodePackages.package.override (old: {
-    buildInputs = old.buildInputs ++ [ pkgs.pkg-config pkgs.libusb ];
-      preRebuild = ''
-        ${pkgs.jq}/bin/jq ".baseUrl |= \"$NODE_PATH\"" < tsconfig.json > tsconfig_temp.json
-        cat tsconfig_temp.json
-        mv tsconfig_temp.json tsconfig.json
-        ${postInstallFixup}
-        npm run prepare
-      '';
-  });
+  yarnPackageNix = pkgs.runCommand "yarn-package.nix" {} ''
+    # We sed hw-app-alamgu to a constant here, so that the package.json can be whatever; we're overriding it anyways.
+    ${yarn2nix}/bin/yarn2nix --template \
+      <(sed 's/"hw-app-alamgu".*$/"hw-app-alamgu": "0.0.1",/' ${./package.json}) \
+      > $out
+  '';
+  nixLib = yarn2nix.nixLib;
 
-  generateCmd = pkgs.writeShellScriptBin "run-node2nix" ''
-    ${pkgs.nodePackages.node2nix}/bin/node2nix --supplement-input supplement.json --composition composed.nix
-  '';
+  localOverrides = self: super:
+      let
+        registries = {
+          yarn = n: v: "https://registry.yarnpkg.com/${n}/-/${n}-${v}.tgz";
+        };
+        y = registries.yarn;
+        s = self;
+      in {
+        "usb@^1.7.0" = {
+          inherit (super."usb@^1.7.0") key;
+          drv = super."usb@^1.7.0".drv.overrideAttrs (attrs: {
+            nativeBuildInputs = [
+              pkgs.python3 nodejs pkgs.libusb1
+            ] ++ lib.optional pkgs.stdenv.hostPlatform.isLinux pkgs.systemd;
+            dontBuild = false;
+            buildPhase = ''
+              ln -s ${nixLib.linkNodeDeps { name=attrs.name; dependencies=attrs.passthru.nodeBuildInputs; }} node_modules
+              ${pkgs.nodePackages.node-gyp}/bin/node-gyp rebuild --nodedir=${lib.getDev nodejs} # /include/node
+            '';
+          });
+        };
+
+        "node-hid@^2.1.2" = {
+          inherit (super."node-hid@^2.1.2") key;
+          drv = super."node-hid@^2.1.2".drv.overrideAttrs (attrs: {
+            nativeBuildInputs = [
+              pkgs.python3 nodejs pkgs.libusb1 pkgs.pkg-config
+            ] ++ lib.optional pkgs.stdenv.hostPlatform.isLinux pkgs.systemd;
+            dontBuild = false;
+            buildPhase = ''
+              ln -s ${nixLib.linkNodeDeps { name=attrs.name; dependencies=attrs.passthru.nodeBuildInputs; }} node_modules
+              ${pkgs.nodePackages.node-gyp}/bin/node-gyp rebuild --nodedir=${lib.getDev nodejs} # /include/node
+            '';
+          });
+        };
+
+        "hw-app-alamgu@0.0.1" = super._buildNodePackage rec {
+          key = "hw-app-alamgu";
+          version = "0.0.1";
+          src = thunkSource ./dep/hw-app-alamgu;
+          buildPhase = ''
+            ln -s $nodeModules node_modules
+            node $nodeModules/.bin/tsc
+            node $nodeModules/.bin/tsc -m ES6 --outDir lib-es
+          '';
+          nodeModules = nixLib.linkNodeDeps {
+            name = "hw-app-alamgu";
+            dependencies = nodeBuildInputs ++ [
+              (s."@types/node@^17.0.10")
+              (s."typescript@^4.5.5")
+            ];
+          };
+          passthru = { inherit nodeModules; };
+          NODE_PATH = nodeModules;
+          nodeBuildInputs = [
+            (s."@ledgerhq/hw-transport@^6.20.0")
+            (s."fast-sha256@^1.3.0")
+            (s."typedoc@^0.22.7")
+          ];
+        };
+
+      };
+
+  deps = nixLib.buildNodeDeps
+    (lib.composeExtensions
+      (pkgs.callPackage yarnDepsNix {
+        fetchgit = builtins.fetchGit;
+      })
+      localOverrides);
+
+  src0 = lib.sources.cleanSourceWith {
+    src = ./.;
+    filter = p: _: let
+      p' = baseNameOf p;
+      srcStr = builtins.toString ./.;
+    in !(lib.elem p' [ "node_modules" ".git" "dep" ]);
+  };
+
+  src = lib.sources.sourceFilesBySuffices src0 [
+    ".js" ".cjs" ".ts" ".json"
+  ];
+in rec {
+  inherit deps yarnDepsNix yarnPackageNix thunkSource;
+
+  testModules = nixLib.buildNodePackage ({
+    src = pkgs.runCommand "package-json" {} ''
+      mkdir $out
+      cp ${./package.json} $out/package.json
+    '';
+  } // nixLib.callTemplate yarnPackageNix deps);
+
+  testPackage = nixLib.buildNodePackage (let
+    self = nixLib.callTemplate yarnPackageNix deps // {
+      inherit src;
+      buildPhase = ''
+        ln -s $nodeModules node_modules
+        node $nodeModules/.bin/tsc
+      '';
+      postInstall = ''
+        mkdir -p $out/bin
+        ln -s "$out/build/cli.js" "$out/bin/generic-cli"
+      '';
+      nodeModules = nixLib.linkNodeDeps {
+        name = "ledger-generic-cli";
+        dependencies = self.nodeBuildInputs ++ [
+          (deps."@types/node@^17.0.10")
+          (deps."typescript@^4.5.5")
+        ];
+      };
+    };
+  in self);
 }
